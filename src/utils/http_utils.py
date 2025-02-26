cd08f338d685554f739cc594867778efa8b0d2f9
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-02-26 14:22:30 +0800

    updated http_utils.py

--- src/utils/http_utils.py
+++ src/utils/http_utils.py
@@ -2,7 +2,7 @@ from fastapi import status
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
-def HTTPResponse(status_code: status, detail: tuple):
+def HTTPResponse(status_code: status, detail: dict | str):
 	return JSONResponse(
 		status_code=status_code,
 		content=jsonable_encoder(detail)
